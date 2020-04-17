a2ead54e0136deb43b06ac05be7fa40346474047
Iftakhar Hasan <hasan.km.iftakhar@gmail.com>
2020-04-17 12:15:19 +0600

    Updates

---
+++ precache-manifest.f9247eb0fd3965ed6faaa34ccbefe393.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad85dcffd77ab2802d0255c4f746f41",
+    "url": "/react-covid19-tracker/index.html"
+  },
+  {
+    "revision": "ed5028909de0c40b7ef3",
+    "url": "/react-covid19-tracker/static/js/2.d0d2ab3f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid19-tracker/static/js/2.d0d2ab3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a66ec536bb9c69f17a",
+    "url": "/react-covid19-tracker/static/js/main.f6cac342.chunk.js"
+  },
+  {
+    "revision": "8dff6f747fb39dc710bc",
+    "url": "/react-covid19-tracker/static/js/runtime-main.fa736840.js"
+  }
+]);
